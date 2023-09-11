2bbc0646d93f741d5d8e1ca58a5c477697ca8a74
Chris Stamper <cstampok@gmail.com>
2023-09-11 12:58:05 -0500

    eight solid test

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (typeof startString !== 'string' || !startString) return '';
 
   let result = '';
 
   for (const item of set) {
     if (item.startsWith(startString)) {
-      result += `${item.slice(startString.length)}-`;
+      result += `${item.replace(startString, '')}-`;
     }
   }
 
